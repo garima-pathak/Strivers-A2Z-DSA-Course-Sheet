9624345c51148cb7162df5ba7e171af565885b1b
Garima <101516550+garima-pathak@users.noreply.github.com>
2025-09-10 13:58:35 +0530

    Create CountingFrequenciesOfArrayElements.java
    added to lec 6

---
+++ Step 01  Learn the basics/Lec 6 Learn Basic Hashing/CountingFrequenciesOfArrayElements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> countFrequencies(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+		int[] arr = new int[nums.length];
+		
+		for (int i = 0; i < nums.length; i++) {
+			int k = 1;
+			if(map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i])+1);
+			}else {
+				map.put(nums[i],k);
+			}
+			
+		}
+		
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            List<Integer> pair = new ArrayList<>();
+            pair.add(entry.getKey());
+            pair.add(entry.getValue());
+            result.add(pair);
+        }
+        
+        return result;
+
+    }
+}
