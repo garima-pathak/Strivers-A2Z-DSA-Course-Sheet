37b666c2aafe3542fe9b3c68c3dfb02f5f1c3a9a
Garima <101516550+garima-pathak@users.noreply.github.com>
2025-09-11 00:57:37 +0530

    Create FindTheHighestLowestFrequencyElement.java
    added to Lec 6

---
+++ Step 01  Learn the basics/Lec 6 Learn Basic Hashing/FindTheHighestLowestFrequencyElement.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int mostFrequentElement(int[] nums) {
+            HashMap<Integer,Integer> map=new HashMap<>();
+            int maxEleFreq=Integer.MIN_VALUE;
+            int maxEle=Integer.MAX_VALUE;
+            for (int i = 0; i < nums.length; i++) {
+			int k = 1;
+			if(map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i])+1);
+			}else {
+				map.put(nums[i],k);
+			}
+			maxEleFreq=Math.max(maxEleFreq,map.get(nums[i]));
+		}
+
+        for(Map.Entry<Integer,Integer> et: map.entrySet()){
+            if(et.getValue()==maxEleFreq){
+                if(et.getKey()<maxEle){
+                    maxEle=et.getKey();
+                }
+            }
+        }
+        return maxEle;
+
+    }
+}
+
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(k)
+*/
