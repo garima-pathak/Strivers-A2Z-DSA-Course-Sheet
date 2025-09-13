57fc87f5def3fa87ffa43787c787b940eef450e7
Garima <101516550+garima-pathak@users.noreply.github.com>
2025-09-13 19:18:25 +0530

    Update 03_InsertionSort.java
    added code

--- Step 02 Learn Important Sorting Techniques/Lec 1: Sorting-I/03_InsertionSort.java
+++ Step 02 Learn Important Sorting Techniques/Lec 1: Sorting-I/03_InsertionSort.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int[] insertionSort(int[] nums) {
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+			int j = i;
+			while (j > 0 && nums[j - 1] > nums[j]) {
+				int temp = nums[j - 1];
+				nums[j - 1] = nums[j];
+				nums[j] = temp;
+				j--;
+			}
+		}
+        return nums;
+    }
+}
 
+
+/*
+Time Complexity = O(n^2) <--- (Worst case, Avg. case), O(n) <--- (Best case) 
+Space Complexity = O(1)
+*/
