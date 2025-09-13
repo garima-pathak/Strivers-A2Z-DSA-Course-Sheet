35b1eba4fcd47a68f1fce5b02d4f24f20d83bcfc
Garima <101516550+garima-pathak@users.noreply.github.com>
2025-09-13 16:36:27 +0530

    Update 01_SelectionSort.java
    modified code selection sort

--- Step 02 Learn Important Sorting Techniques/Lec 1: Sorting-I/01_SelectionSort.java
+++ Step 02 Learn Important Sorting Techniques/Lec 1: Sorting-I/01_SelectionSort.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int[] selectionSort(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++) {
+			int mini=i;
+			for(int j=i+1;j<n;j++) {
+				if(nums[j]<nums[mini]) {
+					mini=j;
+				}
+			}
+			int temp=nums[i];
+			nums[i]=nums[mini];
+			nums[mini]=temp;
+		}
+        return nums;
+    }
+}
 
+
+/*
+Time Complexity = O(n^2) <-----(Best Case,Average Case,Worst Case)
+Space Complexity = O(1)
+*/
