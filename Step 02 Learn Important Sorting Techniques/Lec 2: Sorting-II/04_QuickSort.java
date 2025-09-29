09b3037756c83f21388f339dbfa90dec73e5fb1d
Garima <101516550+garima-pathak@users.noreply.github.com>
2025-09-29 23:59:13 +0530

    Created 04_QuickSort.java
    Quick Sort code added

---
+++ Step 02 Learn Important Sorting Techniques/Lec 2: Sorting-II/04_QuickSort.java
@@ -0,0 +1,44 @@
+public class Quick_Sort {
+
+	public static void main(String[] args) {
+		int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
+		int n = arr.length;
+		quickSort(arr, 0, n - 1);
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void quickSort(int[] arr,int low,int high) {
+		if(low>=high)return;
+		int pivot=qFunc(arr,low,high);
+		quickSort(arr,low,pivot-1);
+		quickSort(arr,pivot+1,high);
+	}
+
+	public static int qFunc(int[] arr,int low,int high) {
+		int pivot=arr[low];
+		int i=low,j=high;
+		while(i<j) {
+			while(arr[i]<=pivot && i<high) {
+				i++;
+			}
+			while(arr[j]>pivot && j>=low) {
+				j--;
+			}
+			if(i<j) {
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+		int temp=arr[j];
+		arr[j]=arr[low];
+		arr[low]=temp;
+		return j;
+	}
+}
+
+
+// Time Complexity = O(n^2), Space Complexity = O(n)
